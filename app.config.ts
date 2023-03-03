5fb07940bfb253371c145bb7fc47a97c8e85ebdc
devin <devindai@zego.im>
2023-03-03 18:15:20 +0800

    feat: 初始化项目

---
+++ app.config.ts
@@ -0,0 +1,28 @@
+export default defineAppConfig({
+  docus: {
+    title: '2030',
+    description: '新式文档化',
+    image: '',
+    socials: {
+      github: 'https://github.com/Jun2030/2030-docs'
+    },
+    aside: {
+      level: 1,
+      exclude: []
+    },
+    header: {
+      title: '2030',
+      logo: false,
+      showLinkIcon: true,
+      fluid: false,
+    },
+    footer: {
+      iconLinks: [],
+      credits: {
+        text: 'Made with ❤️ by ZiJun',
+        icon: '',
+        href: '',
+      }
+    }
+  }
+})
