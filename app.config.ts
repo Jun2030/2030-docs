bfbad50deafbb86cd585ca46dbb6ecd3c6903a18
Jun <zijun2030@163.com>
2023-04-09 21:47:02 +0800

    补充Docker文档 (#1)
    * feat: 新增Docker文档
    
    * feat: 补充Docker文档

--- app.config.ts
+++ app.config.ts
@@ -1,28 +1,28 @@
 export default defineAppConfig({
-  docus: {
-    title: '2030',
-    description: '新式文档化',
-    image: '',
-    socials: {
-      github: 'https://github.com/Jun2030/2030-docs'
-    },
-    aside: {
-      level: 1,
-      exclude: []
-    },
-    header: {
-      title: '2030',
-      logo: false,
-      showLinkIcon: true,
-      fluid: false,
-    },
-    footer: {
-      iconLinks: [],
-      credits: {
-        text: 'Made with ❤️ by ZiJun',
-        icon: '',
-        href: '',
-      }
-    }
-  }
-})
+	docus: {
+		title: '2030',
+		description: '小代码，大世界，多元化知识边界探索',
+		image: '',
+		socials: {
+			github: 'https://github.com/Jun2030/2030-docs',
+		},
+		aside: {
+			level: 1,
+			exclude: [],
+		},
+		header: {
+			title: '2030',
+			logo: false,
+			showLinkIcon: true,
+			fluid: false,
+		},
+		footer: {
+			iconLinks: [],
+			credits: {
+				text: 'Made with ❤️ by ZiJun',
+				icon: '',
+				href: '',
+			},
+		},
+	},
+});
